1d6f1300cb47f12d034247379fed89e0bae87342
WildanGarviandi <garviadiwildan@gmail.com>
2025-01-26 20:33:45 +0000

    Final char walk and idle

--- scripts/game_input_events.gd
+++ scripts/game_input_events.gd
@@ -5,7 +5,7 @@ static var direction: Vector2
 static func movement_input() -> Vector2:
 	if Input.is_action_pressed("walk_left"):
 		direction = Vector2.LEFT
-	elif Input.is_action_just_pressed("walk_right"):
+	elif Input.is_action_pressed("walk_right"):
 		direction = Vector2.RIGHT
 	elif Input.is_action_pressed("walk_up"):
 		direction = Vector2.UP
